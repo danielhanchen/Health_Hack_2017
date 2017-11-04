181c865cfd799510cf330e5d0b64c68662292cd1
Daniel Han-Chen <danielhanchen@gmail.com>
2017-11-04 15:27:31 +1100

    Create Modules.js

---
+++ Modules.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var compression = require('compression');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var dotenv = require('dotenv');
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var moment = require('moment');
+var request = require('request');
+var mocks = require('node-mocks-http');
+
+
+// Load environment variables from .env file
+dotenv.load();
+var app = express();
+mongoose.connect(process.env.MONGODB);
+mongoose.connection.on('error', function() {
+  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+  process.exit(1);
+});
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+var Grant = require('./server/models/Grant');
+
+var grantController = require('./server/controllers/grant');
+
+
+
+app.get('/health/current', grantController.getCurrentAwardedGrants);
+app.get('/health/apply', grantController.getGrantsToApply)
+
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
+app.get('*', function(req, res) {
+  res.redirect('/#' + req.originalUrl);
+});
+
+// Production error handler
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.sendStatus(err.status || 500);
+  });
+}
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
